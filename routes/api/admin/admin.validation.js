a939985e2d09fa7dc3c2dbd393c801a76f982ecf
arienalhaq_ <62146186+arienalhaq@users.noreply.github.com>
2020-05-13 17:14:31 +0700

    Add files via upload

---
+++ routes/api/admin/admin.validation.js
@@ -0,0 +1,24 @@
+const {
+    body,
+    param
+} = require('express-validator');
+const {
+    findByEmail
+} = require('./admin.controller');
+
+exports.validation = [
+    body('email').isEmail().normalizeEmail().withMessage('Email anda tidak valid').custom(value => {
+        return findByEmail(value).then(admin => {
+            if (admin) {
+                return Promise.reject('Email sudah terdaftar');
+            }
+        })
+    }),
+    body('password').isLength({
+        min: 6
+    }).withMessage('Password harus lebih dari 6 karakter')
+]
+
+exports.paramValidation = [
+    param('id').isMongoId().withMessage('Mongo Id Salah')
+]
